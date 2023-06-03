5a8d3a006249bfe4588b70e8328618b634a8bc07
lhendre <lhendre2@github.com>
2023-06-02 19:56:07 -0700

    setting up test

--- run_dqn.py
+++ run_dqn.py
@@ -13,43 +13,44 @@ from env import CatEnv
 
 
 def main(_):
-  environment = wrappers.SinglePrecisionWrapper(CatEnv())
-  environment_spec = specs.make_environment_spec(environment)
+    environment = wrappers.SinglePrecisionWrapper(CatEnv())
+    environment_spec = specs.make_environment_spec(environment)
 
-  network = snt.Sequential([
-      snt.Flatten(),
-      snt.nets.MLP([256, environment_spec.actions.num_values])
-  ])
+    network = snt.Sequential([
+        snt.Flatten(),
+        snt.nets.MLP([256, environment_spec.actions.num_values])
+      ])
+    pers = [.1,.3,.6,.9]
+    for p in pers:
+        agent = dqn.DQN(
+        environment_spec=environment_spec,
+        network=network,
+        target_update_period=50,
+        samples_per_insert=8.,
+        priority_exponent=.9,
+        n_step=1,
+        checkpoint=False,
+        epsilon=0.1,
+        learning_rate=1e-4,
+        )
 
-  agent = dqn.DQN(
-    environment_spec=environment_spec,
-    network=network,
-    target_update_period=50,
-    samples_per_insert=8.,
-    priority_exponent=.9,
-    n_step=1,
-    checkpoint=False,
-    epsilon=0.1,
-    learning_rate=1e-4,
-  )
-
-  loop = acme.EnvironmentLoop(environment, agent)
-  loop.run(num_episodes=20000)  # pytype: disable=attribute-error
+        loop = acme.EnvironmentLoop(environment, agent)
+        loop.run(num_episodes=20000)  # pytype: disable=attribute-error
 
 
-  state = np.zeros((1,55))
-  state[0, 6] = 1.0
-  q_vals = agent._learner._network(tf.constant(state, dtype=tf.float32))
-  print('Starting with C; should see high weight on Si (index 42):')
-  print(q_vals)
+        state = np.zeros((1,55))
+        state[0, 6] = 1.0
+        q_vals = agent._learner._network(tf.constant(state, dtype=tf.float32))
+        print('Starting with C; should see high weight on Si (index 42):')
+        print(q_vals)
 
-  state = np.zeros((1,55))
-  state[0, 23] = 1.0
-  q_vals = agent._learner._network(tf.constant(state, dtype=tf.float32))
-  print('Starting with Mn; should see higher weight on Pd and Pt (indices 32 & 33):')
-  print(q_vals)
+        state = np.zeros((1,55))
+        state[0, 23] = 1.0
+        q_vals = agent._learner._network(tf.constant(state, dtype=tf.float32))
+        print('Starting with Mn; should see higher weight on Pd and Pt (indices 32 & 33):')
+        print(q_vals)
 
-  
+
 
 if __name__ == '__main__':
-  app.run(main)
+    app.run(main)
